837a8a35a3abda7a932f0116f6ffed6550127e84
Disasster27 <50550852+Disasster27@users.noreply.github.com>
2019-10-18 17:11:12 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -214,9 +214,10 @@ function givCount () {
 	} else if (  getPathname ( 'Product.html' ) ) { 
 		new ProductsList( url, 9, cart );
 	}  else {
+		new ProductsList( url, 8, cart );
 		cart.checkCart()
 	}
 } 
 
 givCount()
-console.log(window.location.pathname)
+
